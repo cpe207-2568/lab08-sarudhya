9c409e7bc82625dcd84ffba3c8064392b34ec9f2
sarudhya <sarudhya.achavakul@gmail.com>
2025-08-01 18:13:18 +0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Header } from "./component/Header";
 import { Sidebar } from "./component/Sidebar";
 import { Footer } from "./component/Footer";
 import { TaskInput } from "./component/TaskInput";
+import { TaskCard } from "./component/Taskcard";
 
 function App() {
   return (
@@ -19,62 +20,9 @@ function App() {
             {/* input ด้านบน*/}
               <TaskInput />
             {/* Card รายการ */}
-            <div className="card mb-3">
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-4">
-                    <h5 className="card-title">Read a book</h5>
-                  </div>
-                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-4">
-                    <p className="card-text"> Vite + React + Bootstrap + TS </p>
-                  </div>
-                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-2">
-                    <button className="btn btn-success me-2">Done</button>
-                  </div>
-                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-2">
-                    <button className="btn btn-danger">Delete</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="card mb-3">
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-4">
-                    <h5 className="card-title">Write code</h5>
-                  </div>
-                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-4">
-                    <p className="card-text">Finish project for class</p>
-                  </div>
-                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-2">
-                    <button className="btn btn-success me-2">Done</button>
-                  </div>
-                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-2">
-                    <button className="btn btn-danger">Delete</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="card mb-3">
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-4">
-                    <h5 className="card-title">Deploy app</h5>
-                  </div>
-                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-4">
-                    <p className="card-text">Push project to GitHub Pages</p>
-                  </div>
-                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-2">
-                    <button className="btn btn-success me-2">Done</button>
-                  </div>
-                  <div className="col-xs-3 col-sm-3 col-md-3 col-lg-2">
-                    <button className="btn btn-danger">Delete</button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <TaskCard id={1} title="Read a book" description="Vite + React + Bootstrap + TS" isDone={true}/>
+            <TaskCard id={2} title="Write code" description="Finish project for class" isDone={false} />
+            <TaskCard id={3} title="Deploy app" description="Push project to GitHub Pages" isDone={false} />
           </main>
         </div>
       </div>
--- src/component/Taskcard.tsx
+++ src/component/Taskcard.tsx
@@ -1,13 +1,15 @@
-export function TaskCard(){
+import type { TaskCardProps } from "../libs/types";
+
+export function TaskCard(props: TaskCardProps){
     return(
         <div className="card mb-3">
               <div className="card-body">
                 <div className="row">
                   <div className="col-xs-3 col-sm-3 col-md-3 col-lg-4">
-                    <h5 className="card-title">Read a book</h5>
+                    <h5 className="card-title">{props.title} </h5>
                   </div>
                   <div className="col-xs-3 col-sm-3 col-md-3 col-lg-4">
-                    <p className="card-text"> Vite + React + Bootstrap + TS </p>
+                    <p className="card-text"> {props.description} </p>
                   </div>
                   <div className="col-xs-3 col-sm-3 col-md-3 col-lg-2">
                     <button className="btn btn-success me-2">Done</button>
